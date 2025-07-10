926c94bb4069242e8b9f4ed6630f92950d001f80
whatididitagain <fobuliosmevraner@gmail.com>
2025-07-10 21:46:38 +0500

    newd

--- config/server.js
+++ config/server.js
@@ -1,5 +1,13 @@
 module.exports = ({ env }) => ({
+  host: '0.0.0.0',
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', ['qwerty123secretA', 'zxcvb456secretB']),
+    keys: env.array('APP_KEYS', ['b83KzP1Q#x4V^zXe', 'N2g@fWc%y6H!MsTr']),
   },
 });
+
+
+
+
+
+
